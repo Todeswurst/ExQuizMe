e2d5da3d8a2fef6bc494d7589ec3915dd30a365a
Ejder Das <ejder.das@stud.uni-duisburg-essen.de>
2018-07-19 00:16:41 +0200

    Day One Patch

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,6 @@
 const config = {
-    backend: "https://cryptic-ravine-28694.herokuapp.com/",
-    url: "https://cryptic-bastion-19060.herokuapp.com/"
+    backend: "https://cryptic-ravine-28694.herokuapp.com",
+    url: "https://cryptic-bastion-19060.herokuapp.com"
 }
 
 export default config;
